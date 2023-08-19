fcc6ef115e85650b24987ac003daba9915217832
Johngtka <79154804+Johngtka@users.noreply.github.com>
2023-08-19 12:17:55 +0200

    feat: generally view of app is done (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
 
+import { delay } from 'rxjs/operators';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+
+@UntilDestroy()
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+    constructor(private observer: BreakpointObserver) {}
+
+    @ViewChild(MatSidenav)
+    sidenav!: MatSidenav;
+
+    ngAfterViewInit() {
+        this.observer
+            .observe(['(max-width: 800px)'])
+            .pipe(delay(1), untilDestroyed(this))
+            .subscribe((res) => {
+                if (res.matches) {
+                    this.sidenav.mode = 'over';
+                    this.sidenav.close();
+                } else {
+                    this.sidenav.mode = 'side';
+                    this.sidenav.open();
+                }
+            });
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -37,6 +41,10 @@ const materialsModules = [
     MatFormFieldModule,
     MatProgressSpinnerModule,
     MatMenuModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatTooltipModule,
     MatDialogModule,
     MatSnackBarModule,
 ];
