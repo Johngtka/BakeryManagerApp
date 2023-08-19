a6fc5f7439533e3085f7bbb5923c7be47f63de52
Johngtka <johngtka@gmail.com>
2023-08-19 14:48:26 +0200

    feat: changed api url, added new service and prepare the home component for users

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../models/user';
+import { UsersService } from '../services/users.service';
+import { SnackService, SNACK_TYPE } from '../services/snack.service';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+    ngOnInit(): void {
+        // r
+    }
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: number;
+    name: string;
+    surname: string;
+    email: string;
+    phone: string;
+    nick: string;
+    password: string;
+}
