a271e5d4cc99865e09e59189f0172821a5772ba4
Johngtka <johngtka@gmail.com>
2023-08-19 18:12:47 +0200

    feat: added users table on home component, new service for users data and new model

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MatTableDataSource } from '@angular/material/table';
+
 import { User } from '../models/user';
 import { UsersService } from '../services/users.service';
 import { SnackService, SNACK_TYPE } from '../services/snack.service';
@@ -10,7 +12,28 @@ import { SnackService, SNACK_TYPE } from '../services/snack.service';
     styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+    constructor(
+        private userService: UsersService,
+        private snackService: SnackService,
+    ) {}
+
+    user!: User[];
+    dataSource!: MatTableDataSource<User>;
+    displayedColumns: string[] = ['name', 'surname', 'nick', 'phone', 'email'];
+
     ngOnInit(): void {
-        // r
+        this.userService.getUsers().subscribe({
+            next: (data) => {
+                this.user = data;
+                this.dataSource = new MatTableDataSource<User>(this.user);
+            },
+            error: (err) => {
+                this.snackService.showSnackBar(
+                    'ERRORS.USERS_GETTING_ERROR',
+                    SNACK_TYPE.error,
+                );
+                console.log(err);
+            },
+        });
     }
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { User } from '../models/user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class UsersService {
+    constructor(private http: HttpClient) {}
+    private apiUrl = environment.API_URL;
 
-  constructor() { }
+    getUsers(): Observable<Array<User>> {
+        return this.http.get<Array<User>>(`${this.apiUrl}`);
+    }
 }
