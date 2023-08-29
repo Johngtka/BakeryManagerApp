95126ec0c41722c76f4bc9cad30050f7422abcc8
Johngtka <johngtka@gmail.com>
2023-08-29 12:10:46 +0200

    feat: added table with products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,8 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { MatTableDataSource } from '@angular/material/table';
+
+import { Product } from '../models/product';
+import { ProductsService } from '../services/products.service';
+import { SnackService, SNACK_TYPE } from '../services/snack.service';
 
 @Component({
     selector: 'app-products',
     templateUrl: './products.component.html',
     styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent {}
+export class ProductsComponent implements OnInit {
+    constructor(
+        private productService: ProductsService,
+        private snackService: SnackService,
+    ) {}
+
+    product!: Product[];
+    logID: number[] = [];
+    dataSource!: MatTableDataSource<Product>;
+    loadingProcess: boolean = true;
+    isSelected: boolean = false;
+    displayedColumns: string[] = [
+        'name',
+        'unitPrice',
+        'components',
+        'description',
+    ];
+
+    ngOnInit(): void {
+        this.productService.getProducts().subscribe({
+            next: (data) => {
+                this.product = data;
+                this.dataSource = new MatTableDataSource<Product>(this.product);
+                this.loadingProcess = false;
+            },
+            error: (err) => {
+                this.snackService.showSnackBar(
+                    'ERRORS.PRODUCTS_GETTING_ERROR',
+                    SNACK_TYPE.error,
+                );
+                console.log(err);
+            },
+        });
+    }
+}
