36c88a98acced518e54a6901c8defc0ef25bc958
Johngtka <johngtka@gmail.com>
2023-08-30 11:35:39 +0200

    feat: added basic functionality of pipes and implement pipes to table

--- src/app/pipes/price.pipe.ts
+++ src/app/pipes/price.pipe.ts
@@ -4,7 +4,14 @@ import { Pipe, PipeTransform } from '@angular/core';
     name: 'price',
 })
 export class PricePipe implements PipeTransform {
-    transform(value: unknown, ...args: unknown[]): unknown {
-        return null;
+    transform(value: string): string {
+        const priceProduct = parseInt(value, 10);
+        if (priceProduct > 1) {
+            return value + 'zł';
+        } else if (priceProduct < 1) {
+            return '80gr';
+        } else {
+            return '';
+        }
     }
 }
--- src/app/pipes/weight.pipe.ts
+++ src/app/pipes/weight.pipe.ts
@@ -4,7 +4,15 @@ import { Pipe, PipeTransform } from '@angular/core';
     name: 'weight',
 })
 export class WeightPipe implements PipeTransform {
-    transform(value: unknown, ...args: unknown[]): unknown {
-        return null;
+    transform(value: string): string {
+        const weight = parseInt(value, 10);
+        const mass = weight.toString().replace(/0/g, '');
+        if (weight >= 1000) {
+            return mass + 'kg';
+        } else if (weight <= 150) {
+            return value + 'g';
+        } else {
+            return '';
+        }
     }
 }
