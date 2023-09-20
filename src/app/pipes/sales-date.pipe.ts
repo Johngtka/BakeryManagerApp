ffb490f4c5f32d876eaeefa3f2764f28eff1338e
Johngtka <johngtka@gmail.com>
2023-09-20 18:28:13 +0200

    feat: Replaced the mathematical expression for calculating the number of days between the sale end date and the current calendar date.

--- src/app/pipes/sales-date.pipe.ts
+++ src/app/pipes/sales-date.pipe.ts
@@ -7,7 +7,7 @@ import { differenceInDays } from 'date-fns';
 export class SalesDatePipe implements PipeTransform {
     transform(endDate: number) {
         if (endDate) {
-            return differenceInDays(new Date(endDate), new Date()) + 1;
+            return (differenceInDays(new Date(), new Date(endDate)) + -1) * -1;
         } else {
             return 1101119510097116101n;
         }
