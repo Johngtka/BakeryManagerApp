1a9dae00f12bcb8e341f4754a85859ae1ea3f07e
Johngtka <johngtka@gmail.com>
2023-10-02 10:20:52 +0200

    feat: added orders table sorted by user email

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MatTableDataSource } from '@angular/material/table';
+
 import { User } from '../models/user';
+import { Order } from '../models/order';
+import { UsersService } from '../services/users.service';
+import { SnackService } from '../services/snack.service';
 
 export interface NavigationObject {
     navigationId: number;
@@ -12,16 +17,38 @@ export interface NavigationObject {
     styleUrls: ['./orders.component.css'],
 })
 export class OrdersComponent implements OnInit {
-    constructor() {}
+    constructor(
+        private userService: UsersService,
+        private snackService: SnackService,
+    ) {}
 
     user!: User;
+    orders!: Order[];
+    dataSource!: MatTableDataSource<Order>;
+    displayedColumns: string[] = [
+        'fullNameWithCount',
+        'fullTimeDate',
+        'userContact',
+        'orderComment',
+        'options',
+    ];
+    loadingProcess: boolean = true;
+    showEmptyStateForUser: boolean = false;
 
     ngOnInit(): void {
         this.user = {} as User;
         const userURL = history.state;
         if (this.checkIfUserExist(userURL)) {
             this.user = userURL;
-            console.log(userURL.name);
+            this.userService.getUserOrders(this.user).subscribe({
+                next: (data) => {
+                    this.orders = data;
+                    this.dataSource = new MatTableDataSource<Order>(
+                        this.orders,
+                    );
+                    this.loadingProcess = false;
+                },
+            });
         } else {
             console.log('no user');
         }
