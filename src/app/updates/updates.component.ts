805a14845d23070b71058f2281ec1e74ba722bc0
Johngtka <johngtka@gmail.com>
2024-05-31 12:56:39 +0200

    fix: commented not needed code

--- src/app/updates/updates.component.ts
+++ src/app/updates/updates.component.ts
@@ -80,21 +80,8 @@ export class UpdatesComponent implements AfterViewInit, OnInit {
         });
         dialogRef.afterClosed().subscribe((result: Update) => {
             if (result) {
-                this.updateTable(result);
-                this.updateService.getUpdates().subscribe({
-                    next: (data) => {
-                        this.dataSource = new MatTableDataSource<Update>(data);
-                        this.dataSource.paginator = this.paginator;
-                        this.loadingProcess = false;
-                    },
-                    error: (err) => {
-                        console.log(err);
-                        this.snackService.showSnackBar(
-                            'ERRORS.UPDATES_GETTING_ERROR',
-                            SNACK_TYPE.error,
-                        );
-                    },
-                });
+                // this.updateTable(result);
+                this.getUpdates();
             }
         });
     }
@@ -129,24 +116,24 @@ export class UpdatesComponent implements AfterViewInit, OnInit {
         });
     }
 
-    private updateTable(newOrUpdatedLogs: Update): void {
-        if (!!this.update && !!newOrUpdatedLogs) {
-            const tableUpdateIndex = this.update.findIndex(
-                (li: Update) => li.id === newOrUpdatedLogs.id,
-            );
-            // li it is shortcut of log id
-            if (tableUpdateIndex !== -1) {
-                // update
-                this.update[tableUpdateIndex] = newOrUpdatedLogs;
-                this.update = [...this.update];
-                this.dataSource = new MatTableDataSource<Update>(this.update);
-                this.dataSource.paginator = this.paginator;
-            } else {
-                // new
-                this.update = [...this.update, newOrUpdatedLogs];
-                this.dataSource = new MatTableDataSource<Update>(this.update);
-                this.dataSource.paginator = this.paginator;
-            }
-        }
-    }
+    // private updateTable(newOrUpdatedLogs: Update): void {
+    //     if (!!this.update && !!newOrUpdatedLogs) {
+    //         const tableUpdateIndex = this.update.findIndex(
+    //             (li: Update) => li.id === newOrUpdatedLogs.id,
+    //         );
+    //         // li it is shortcut of log id
+    //         if (tableUpdateIndex !== -1) {
+    //             // update
+    //             this.update[tableUpdateIndex] = newOrUpdatedLogs;
+    //             this.update = [...this.update];
+    //             this.dataSource = new MatTableDataSource<Update>(this.update);
+    //             this.dataSource.paginator = this.paginator;
+    //         } else {
+    //             // new
+    //             this.update = [...this.update, newOrUpdatedLogs];
+    //             this.dataSource = new MatTableDataSource<Update>(this.update);
+    //             this.dataSource.paginator = this.paginator;
+    //         }
+    //     }
+    // }
 }
