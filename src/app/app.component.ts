4ed6d6da4e7f4d7c6cb77f5c2057b29946ecee7b
Johngtka <johngtka@gmail.com>
2024-11-03 19:29:39 +0100

    feat: added full html template of login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
+import { FormControl } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
 
 import { MatSidenav } from '@angular/material/sidenav';
 
 import { delay } from 'rxjs/operators';
+import { Employers } from './models/employers';
 import { environment } from 'src/environments/environment';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
@@ -20,9 +23,11 @@ export class AppComponent implements AfterViewInit, OnInit {
     date = new Date();
     version = environment.appVersion;
     fullDateValue!: string;
+    employersForm!: FormGroup;
     dayScopeValue!: string | number;
     isMobileDetected = false;
     monthScopeValue!: string | number;
+    originalFormValues!: Employers;
     showEmployersLoginPage = true;
 
     ngOnInit(): void {
@@ -41,6 +46,11 @@ export class AppComponent implements AfterViewInit, OnInit {
         this.dayScopeValue = day < 10 ? '0' + day : day;
         this.monthScopeValue = month < 10 ? '0' + month : month;
         this.fullDateValue = `${this.dayScopeValue}.${this.monthScopeValue}.${year}`;
+        this.employersForm = new FormGroup({
+            login: new FormControl('', [Validators.required]),
+            password: new FormControl('', [Validators.required]),
+        });
+        this.originalFormValues = this.employersForm.value;
     }
 
     ngAfterViewInit(): void {
@@ -60,5 +70,12 @@ export class AppComponent implements AfterViewInit, OnInit {
 
     logEmployer() {
         this.showEmployersLoginPage = false;
+    }
+
+    hasChange(): boolean | void {
+        return (
+            JSON.stringify(this.employersForm.value) !==
+            JSON.stringify(this.originalFormValues)
+        );
     }
 }
---
+++ src/app/models/employers.ts
@@ -0,0 +1,4 @@
+export interface Employers {
+    name: string;
+    password: string;
+}
