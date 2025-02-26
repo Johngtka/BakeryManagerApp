d0bc50116468f3d00dd444cae9b66a0716215ab5
Johngtka <johngtka@gmail.com>
2025-02-26 16:29:09 +0100

    feat: added html table in emm module

--- src/app/emm/emm.component.ts
+++ src/app/emm/emm.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { MatTableDataSource } from '@angular/material/table';
+
+import { Employers } from '../models/employers';
+import { SnackService, SNACK_TYPE } from '../services/snack.service';
+import { EmployersService } from '../services/employers.service';
 
 @Component({
     selector: 'app-emm',
     templateUrl: './emm.component.html',
     styleUrl: './emm.component.css',
 })
-export class EMMComponent {}
+export class EMMComponent implements OnInit {
+    constructor(
+        private employerService: EmployersService,
+        private snackService: SnackService,
+    ) {}
+
+    dataSource!: MatTableDataSource<Employers>;
+    loadingProcess: boolean = true;
+    displayedColumns: string[] = ['login', 'password', 'email', 'position'];
+    showLostConnection!: boolean;
+
+    ngOnInit(): void {}
+
+    private getEmployers(): void {}
+}
