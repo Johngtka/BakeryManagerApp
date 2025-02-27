4c3d2d3126154d4ba3c3d5809e99250a1e415c43
Johngtka <johngtka@gmail.com>
2025-02-27 09:36:22 +0100

    feat:added basic table functionality with clear button and css for mobiles with options from products

--- src/app/emm/emm.component.ts
+++ src/app/emm/emm.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+    trigger,
+    state,
+    style,
+    animate,
+    transition,
+} from '@angular/animations';
 
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
@@ -11,6 +18,42 @@ import { EmployersService } from '../services/employers.service';
     selector: 'app-emm',
     templateUrl: './emm.component.html',
     styleUrl: './emm.component.css',
+    animations: [
+        trigger('editHovered', [
+            state(
+                'inactive',
+                style({
+                    background: 'white',
+                }),
+            ),
+            state(
+                'active',
+                style({
+                    background: 'green',
+                    color: 'white',
+                }),
+            ),
+            transition('inactive => active', animate('240ms ease-in')),
+            transition('active => inactive', animate('400ms ease-out')),
+        ]),
+        trigger('deleteHovered', [
+            state(
+                'inactive',
+                style({
+                    background: 'white',
+                }),
+            ),
+            state(
+                'active',
+                style({
+                    background: 'red',
+                    color: 'white',
+                }),
+            ),
+            transition('inactive => active', animate('240ms ease-in')),
+            transition('active => inactive', animate('400ms ease-out')),
+        ]),
+    ],
 })
 export class EMMComponent implements OnInit {
     constructor(
@@ -19,14 +62,36 @@ export class EMMComponent implements OnInit {
     ) {}
 
     @ViewChild(MatPaginator) paginator!: MatPaginator;
+    empID: number[] = [];
     employer!: Employers[];
     dataSource!: MatTableDataSource<Employers>;
+    isEditHovered: string = 'inactive';
     loadingProcess: boolean = true;
-    displayedColumns: string[] = ['login', 'password', 'email', 'position'];
+    isDeleteHovered: string = 'inactive';
+    displayedColumns: string[] = [
+        'login',
+        'password',
+        'email',
+        'position',
+        'options',
+    ];
     showLostConnection!: boolean;
 
     ngOnInit(): void {
         this.getEmployers();
+    }
+
+    clearSelect(): void {
+        this.empID = [];
+    }
+
+    clickedRow(row: Employers): void {
+        const ID = this.empID.indexOf(row.id);
+        if (ID !== -1) {
+            this.empID.splice(ID, 1);
+        } else {
+            this.empID.push(row.id);
+        }
     }
 
     private getEmployers(): void {
